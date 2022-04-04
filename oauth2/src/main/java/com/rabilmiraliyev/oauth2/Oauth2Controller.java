dcfcac17a223617ba3a864bc10e37e7c4995ece2
miraliyevrabil <miraliyevr@gmail.com>
2022-04-05 00:52:07 +0400

    oauht2

--- oauth2/src/main/java/com/rabilmiraliyev/oauth2/Oauth2Controller.java
+++ oauth2/src/main/java/com/rabilmiraliyev/oauth2/Oauth2Controller.java
@@ -1,10 +1,12 @@
 package com.rabilmiraliyev.oauth2;
 
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class Oauth2Controller {
 
     @GetMapping("/")
--- oauth2/src/main/java/com/rabilmiraliyev/oauth2/SecurityConfig.java
+++ oauth2/src/main/java/com/rabilmiraliyev/oauth2/SecurityConfig.java
@@ -1,20 +1,23 @@
 package com.rabilmiraliyev.oauth2;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
+    @Autowired
+    private Oauth2UserService oauth2UserService;
     @Override
     public void configure(HttpSecurity http) throws Exception {
-
         http
                 .antMatcher("/**").authorizeRequests()
                 .antMatchers("/").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .oauth2Login();
+//                .userInfoEndpoint()
+//                .userService(oauth2UserService);
     }
 }
