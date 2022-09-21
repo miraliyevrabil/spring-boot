7343912831f3d359f108cb216e38b9ffbdb7b067
miraliyevrabil <miraliyevr@gmail.com>
2022-09-21 10:39:47 +0400

    generator qr code

---
+++ genetate-qr-code/src/main/java/com/rabilmiraliyev/generateqrcode/QRCodeGenerator.java
@@ -0,0 +1,39 @@
+package com.rabilmiraliyev.generateqrcode;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+public class QRCodeGenerator {
+
+    static String data = "https://www.linkedin.com/in/rabilmiraliyev/";
+
+    public static void generateQRCodeImage( int width, int height, String filePath)
+            throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(data, BarcodeFormat.QR_CODE, width, height);
+
+        Path path = FileSystems.getDefault().getPath(filePath);
+        MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
+
+    }
+
+
+    public static byte[] getQRCodeImage( int width, int height) throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(data, BarcodeFormat.QR_CODE, width, height);
+
+        ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
+        MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream);
+        byte[] pngData = pngOutputStream.toByteArray();
+        return pngData;
+    }
+
+}
---
+++ genetate-qr-code/src/test/java/com/rabilmiraliyev/generateqrcode/GenerateQrCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabilmiraliyev.generateqrcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenerateQrCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
