c039b5db832f338e94fbbf7f7e868ac830a9f466
miraliyevrabil <miraliyevr@gmail.com>
2023-04-23 03:27:38 +0400

    optimize imports

--- elasticsearch/src/main/java/com/rabilmiraliyev/ElasticsearchApplication.java
+++ elasticsearch/src/main/java/com/rabilmiraliyev/ElasticsearchApplication.java
@@ -3,7 +3,6 @@ package com.rabilmiraliyev;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @SpringBootApplication(exclude = ElasticsearchDataAutoConfiguration.class)
 public class ElasticsearchApplication {
